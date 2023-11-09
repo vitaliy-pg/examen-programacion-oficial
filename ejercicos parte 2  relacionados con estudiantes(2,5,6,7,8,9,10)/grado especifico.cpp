aa09b2441f09b3840ad3d40f20c28dc221e23824
vitaliy-pg <aljndrvt@myuax.com>
2023-11-09 13:02:50 +0100

    creo un vector para el grado de los estudiantes  y una funcion principal con nombres de aulmnos y que me de cual de ellos esta en el 5 grado

--- ejercicos parte 2  relacionados con estudiantes(2,5,6,7,8,9,10)/grado especifico.cpp
+++ ejercicos parte 2  relacionados con estudiantes(2,5,6,7,8,9,10)/grado especifico.cpp
@@ -18,5 +18,38 @@ private:
     std::string nombre;
     int grado;
 };
+std::vector<Estudiante> filtrarEstudiantesPorGrado(const std::vector<Estudiante>& estudiantes, int gradoDeseado) {
+    std::vector<Estudiante> estudiantesFiltrados;
+    for (const Estudiante& estudiante : estudiantes) {
+        if (estudiante.getGrado() == gradoDeseado) {
+            estudiantesFiltrados.push_back(estudiante);
+        }
+    }
+    return estudiantesFiltrados;
+}
 
+int main() {
+    // Crear un vector de estudiantes
+    std::vector<Estudiante> estudiantes = {
+            Estudiante("Juan", 5),
+            Estudiante("María", 6),
+            Estudiante("Pedro", 5),
+            Estudiante("Luis", 7),
+            Estudiante("Ana", 6)
+    };
+
+    // Grado que deseas filtrar
+    int gradoDeseado = 5;
+
+    // Filtrar estudiantes por grado
+    std::vector<Estudiante> estudiantesFiltrados = filtrarEstudiantesPorGrado(estudiantes, gradoDeseado);
+
+    // Imprimir los estudiantes del grado deseado
+    std::cout << "Estudiantes del grado " << gradoDeseado << ":" << std::endl;
+    for (const Estudiante& estudiante : estudiantesFiltrados) {
+        std::cout << estudiante.getNombre() << std::endl;
+    }
+
+    return 0;
+}
 
