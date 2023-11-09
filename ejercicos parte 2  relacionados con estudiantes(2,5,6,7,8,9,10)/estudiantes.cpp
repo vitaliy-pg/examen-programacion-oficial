5dfe755ba28cae133b09fd9aa413a40a7a5190a1
vitaliy-pg <aljndrvt@myuax.com>
2023-11-09 12:17:23 +0100

    utilizo las funciones void mostrar_info_profesor para mostrar la informacion sobre el profesor, tambien utilizo la funcion void imprimir_info_profesor para imprimir los datos ingresados al programa

--- ejercicos parte 2  relacionados con estudiantes(2,5,6,7,8,9,10)/estudiantes.cpp
+++ ejercicos parte 2  relacionados con estudiantes(2,5,6,7,8,9,10)/estudiantes.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include<string>
+#include <vector>
+#include <list>
+#include <numeric>
+using namespace std;
+class Profesor {
+public:
+    Profesor(const std::string& nombre, int edad, const std::string& materia, int experiencia)
+            : nombre_(nombre), edad_(edad), materia_(materia), experiencia_(experiencia) {}
 
+    void mostrar_info_profesor() {
+        std::cout << "Informacion del Profesor:" << std::endl;
+        std::cout << "Nombre: " << nombre_ << std::endl;
+        std::cout << "Edad: " << edad_ << std::endl;
+        std::cout << "Materia que imparte: " << materia_ << std::endl;
+        std::cout << "Años de experiencia: " << experiencia_ << std::endl;
+    }
+
+    void imprimir_info_profesor() {
+        std::cout << "Profesor: " << nombre_ << ", Edad: " << edad_
+                  << ", Materia: " << materia_ << ", Años de experiencia: " << experiencia_ << std::endl;
+    }
+
+private:
+    std::string nombre_;
+    int edad_;
+    std::string materia_;
+    int experiencia_;
+};
