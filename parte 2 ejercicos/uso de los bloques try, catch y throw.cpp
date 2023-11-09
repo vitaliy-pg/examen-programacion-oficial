241067265a350de4ff1ab7b65534d37d716c23a4
vitaliy-pg <aljndrvt@myuax.com>
2023-11-09 11:59:44 +0100

    utilizo los bloques try, catch and trow para manejar la excepcion de que si la variable d= 0 de error de imediato para manejar la excepcion de division por cero no permitida

--- parte 2 ejercicos/uso de los bloques try, catch y throw.cpp
+++ parte 2 ejercicos/uso de los bloques try, catch y throw.cpp
@@ -9,3 +9,15 @@ int main() {
 
     std::cout << "Introduce el denominador: ";
     std::cin >> d;
+    try {
+        if (d == 0) {
+            throw std::runtime_error("División por cero no permitida");
+        }
+        resultado = n/ d;
+        std::cout << "Resultado de la division: " << resultado << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    cout.precision(6);
+    return 0;
+}
