41eacba81367f7e5b14366ba95b354c3426b776d
vitaliy-pg <aljndrvt@myuax.com>
2023-11-09 11:53:07 +0100

    utilizo la funcion intercambiar (&x, &y) para que funcione el programa

--- parte 2 ejercicos/punteros.cpp
+++ parte 2 ejercicos/punteros.cpp
@@ -9,3 +9,13 @@ void intercambiar(int* a, int* b) {
 int main() {
     int x = 5;
     int y = 10;
+    cout << "Valores originales:" <<endl;
+    cout << "x = " << x << ", y = " << y <<endl;
+
+    intercambiar(&x, &y);
+
+    cout << "Valores intercambiados:" <<endl;
+    cout << "x = " << x << ", y = " << y <<endl;
+
+    return 0;
+}
