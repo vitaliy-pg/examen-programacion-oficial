f0e22fdd11a060cb7bd3ce2b18e5bdf6bdbbc6a3
vitaliy-pg <aljndrvt@myuax.com>
2023-11-09 13:13:33 +0100

    los valor de a y b me devuelven su suma

--- parte 2 ejercicos/operaciones matemáticas básicas.cpp
+++ parte 2 ejercicos/operaciones matemáticas básicas.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-
+int a,b, resultado = 0;
+cout<<"Introduce valor de a: ";
+cin>>a;
+cout<<"Introduce valor de b : ";
+cin>>b;
+resultado = a+b;
+cout<<"El resultado de la suma es: "<<resultado<<endl;
     return 0;
 }
