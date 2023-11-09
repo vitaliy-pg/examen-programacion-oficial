05058df6558358ccd7ed4dde4edb9fa03e316ff9
vitaliy-pg <aljndrvt@myuax.com>
2023-11-09 11:57:40 +0100

    pido al programa que me pida introducir los valores de las variables

--- parte 2 ejercicos/uso de los bloques try, catch y throw.cpp
+++ parte 2 ejercicos/uso de los bloques try, catch y throw.cpp
@@ -4,3 +4,8 @@
 using namespace std;
 int main() {
     float n, d, resultado = 0;
+    std::cout << "Introduce el numerador: ";
+    std::cin >> n;
+
+    std::cout << "Introduce el denominador: ";
+    std::cin >> d;
