05c70d4fc559006b3f9d655f2eb858ae506d7040
vitaliy-pg <aljndrvt@myuax.com>
2023-11-09 12:47:24 +0100

    creo una clase materia para registar la excepcion

--- ejercicos parte 2  relacionados con estudiantes(2,5,6,7,8,9,10)/gestion de excepcion personalizada.cpp
+++ ejercicos parte 2  relacionados con estudiantes(2,5,6,7,8,9,10)/gestion de excepcion personalizada.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <set>
+
+class MateriaRegistradaExcepcion : public std::exception {
+public:
+    MateriaRegistradaExcepcion(const std::string& materia) : materia_("La materia '" + materia + "' ya ha sido registrada.") {}
+
+    const char* what() const noexcept override {
+        return materia_.c_str();
+    }
+
+private:
+    std::string materia_;
+};
 
